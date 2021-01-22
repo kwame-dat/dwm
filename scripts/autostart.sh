e66c6460b948a81f23c44f986d658fcf9230caf4
Tony Ampomah <tony@arksolutions.it>
2021-01-22 17:01:07 +0000

    Update autostart

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-function run {
-  if ! pgrep $1 ;
-  then
-    $@&
-  fi
-}
-run multi-monitor.sh &
+multi-monitor &
 # xrandr --output eDP1 --auto --output DP1 --auto --scale 2x2 --right-of eDP1 &
 
 # cursor active at boot
@@ -19,31 +13,23 @@ xinput --set-prop "SYNA2393:00 06CB:7A13 Touchpad" "libinput Accel Speed" 1 &
 
 # Keyboard changes
 ## make CapsLock behave like Ctrl:
-setxkbmap -option ctrl:nocaps
+setxkbmap -option ctrl:nocaps &
 
 ## make short-pressed Ctrl behave like Escape:
-xcape -e 'Control_L=Escape' -t 300
-
-## make short-pressed shift behave like Escape:
-xcape -e 'Shift_L=Escape' -t 300
-xcape -e 'Shift_R=Escape' -t 300
-
-## make short-pressed tab behave like Hyper:
-xcape -e "Hyper_L=Tab;Hyper_R=Return"
+xcape -e 'Control_L=Escape'
 
 ## Increase keyboard key repeat
 xset r rate 300 80 &
 
-xrdb ~/.Xresources
+xrdb ~/.Xresources &
 
 # Cursor active at boot
 xsetroot -cursor_name left_ptr &
 
 # Starting utility applications at boot time
-run variety &
-run nm-applet &
-run xfce4-power-manager &
-numlockx on &
+variety &
+nm-applet &
+xfce4-power-manager &
 picom &
 dunst &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
@@ -53,7 +39,8 @@ dwmblocks &
 # starting user applications at boot time
 /usr/bin/emacs --daemon &
 copyq &
-run nextcloud &
-run syncthing -no-browser &
-run sudo mount -a &
-run kdeconnect-indicator &
+nextcloud &
+syncthing -no-browser &
+kdeconnect-indicator &
+caffeine-indicator &
+sudo mount -a &
